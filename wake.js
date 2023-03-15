45a1f5e0588932c68bbee177d9de5c88f86763e8
Chris <chris.hendricksonacc@gmail.com>
2023-03-15 23:40:45 +1300

    modify wake

--- wake.js
+++ wake.js
@@ -1,10 +1,14 @@
 const wake = async () =>{
     const res = await fetch('https://cookebook.onrender.com/api/internal/wakeup',{
-        method:'GET'
+        method:'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            'RETURN_URL':'https://christopherhendrickson.dev'
+        })
     })
+    console.log(res.status)
 }
 
 wake()
-setInterval(()=>{
-    wake()
-},850000)
