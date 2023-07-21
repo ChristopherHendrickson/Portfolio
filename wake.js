779431ad52c6a9b7b53f05474d596e48d1ef2f15
Chris <chris.hendricksonacc@gmail.com>
2023-07-21 17:21:13 +1200

    add wake

--- wake.js
+++ wake.js
@@ -1,5 +1,9 @@
 const wake = async () =>{
-    const res = await fetch('https://cookebook.onrender.com/api/internal/wakeup',{
+    await fetch('https://cookebook.onrender.com/api/internal/wakeup',{
+        method:'GET',
+        mode:'no-cors',
+    })
+    await fetch('https://osflip.onrender.com/api/internal/wakeup',{
         method:'GET',
         mode:'no-cors',
     })
